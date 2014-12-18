075bf69507c2a2393773237f6efa72707c1c0ccb
liuin <20893791@qq.com>
2014-12-18 15:56:43 +0800

    1218

---
+++ src/StartScene.js
@@ -0,0 +1,48 @@
+var StartLayer = cc.Layer.extend({
+    ctor:function () {
+        this._super();
+ 
+        var size = cc.winSize;
+        
+       
+        
+        //add bg
+        this.bgSprite = new cc.Sprite(res.spbg);
+        this.bgSprite.attr({
+            x: size.width / 2,
+            y: size.height / 2,
+        });
+        this.addChild(this.bgSprite, 0);
+ 
+        //add start menu
+        var helloLabel = new cc.LabelTTF("开始游戏", "微软雅黑", 38);
+
+        var startItem = new cc.MenuItemLabel(
+          helloLabel,
+          function () {
+            cc.log("Menu is clicked!");
+          }, this);
+
+        startItem.attr({
+          x: size.width/2,
+          y: size.height/2,
+          anchorX: 0.5,
+          anchorY: 0.5
+        });
+         
+        var menu = new cc.Menu(startItem);
+        menu.x = 0;
+        menu.y = 0;
+        this.addChild(menu, 1);
+
+        return true;
+    }
+});
+ 
+var StartScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new StartLayer();
+        this.addChild(layer);
+    }
+});
---
+++ src/resource.js
@@ -0,0 +1,24 @@
+var s_HelloWorld = "HelloWorld.png";
+var s_CloseNormal = "HelloWorld.png";
+var s_CloseSelected = "HelloWorld.png";
+
+var res = {
+    spbg : s_HelloWorld
+};
+
+var g_resources = [
+    //image
+    s_HelloWorld,
+    s_CloseNormal,
+    s_CloseSelected
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
